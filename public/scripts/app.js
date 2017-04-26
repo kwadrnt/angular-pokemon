d7f2bb1898baf8f58fd92c4ca197112c4329b7fe
kwadrnt <kevinctse@gmail.com>
2017-04-26 15:58:16 -0700

    filled out PokemonsIndexController

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,23 @@
 angular
-  .module('pokemonApp', [])
-  
+  .module('pokemonApp', ['ngRoute'])
+  .config(config)
+
+  config.$inject = ['$routeProvider', '$locationProvider'];
+
+  function config ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '/templates/pokemons',
+        controllerAs: 'pokemonsIndexCtrl',
+        controller: 'PokemonsIndexController'
+      })
+      .when('/albums/:id', {
+        templateUrl: '/templates/pokemons-show',
+        controllerAs: 'pokemonsShowCtrl',
+        controller: 'PokemonsController'
+      })
+
+      $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
